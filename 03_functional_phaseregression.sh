beb3406194ca009e3e5a5e1ca15d026707e1fe62
idevicente <65595595+idevicente@users.noreply.github.com>
2020-08-19 23:59:16 +0200

    Update 03_functional_phaseregression.sh

--- 03_functional_phaseregression.sh
+++ 03_functional_phaseregression.sh
@@ -2,7 +2,6 @@
 #$ -cwd
 #$ -m be
 #$ -M idevicente@bcbl.eu
-#$ -q long.q
 
 # ******************************************************
 # This script takes 2 arguments: Subject ID, PRJDIR path
@@ -29,22 +28,19 @@ set -e
 
 cd ${PRJDIR}/PREPROC/${SUBJ}/func
 
-
 TR_COUNTS=$(3dinfo -nt ${SUBJ}.magnitude.pb00_tcat.nii.gz)
 TR=$(3dinfo -TR ${SUBJ}.magnitude.pb00_tcat.nii.gz)
 POLORTORDER=$(echo "scale=3;(${TR_COUNTS}*${TR})/150" | bc | xargs printf "%.*f\n" 0)
 POLORTORDER=$(echo "1+${POLORTORDER}" | bc)
 
-
 ################ ORTHOGONAL DISTANCE REGRESSION (ODR) #####################
 
 # We first need to find the respiration peak which will serve as the high pass filter in the ODR
-
 # Compute the power spectrum map of the phase
 
 3dPeriodogram -prefix phase.powerspectrum.nii.gz ${SUBJ}.phase.pb09_detrended_masked.nii.gz -overwrite
 
-# This command finds the time point index of the maximum frequency peak starting from the 93th time point. 
+# The command below finds the time point index of the maximum frequency peak starting from the 93th time point. 
 # We chose the 93th time point because it corresponds to a frequency of 0.25 Hz.
 # Note that the output will not take this into account, and thus we must add a value of 93 in later steps to obtain the real time point index
 
@@ -60,8 +56,7 @@ Freq_peak_index=$(1deval -overwrite -a maxfreqmode.1D[1] -expr '1.1*(a+93)*0.002
 
 echo ${Freq_peak_index}
 
-
-odr(){
+ODR(){
 
 	echo -e "\e[34m ######################################################################################################## \e[39m"
 	
@@ -100,15 +95,13 @@ odr(){
 
 }
 
-#odr
+ODR
 
 #_______________________________________________________________________________________________________________#
 
-
 ################ LEAST SQUARES REGRESSION WITH 3DTPROJECT #####################
 
-
-Lsqrs() {
+OLS() {
 
 	echo -e "\e[34m ######################################################################################################## \e[39m"
 
@@ -120,17 +113,9 @@ Lsqrs() {
 	3dTproject -input ${SUBJ}.magnitude.pb03_volreg_detrended_masked.nii.gz -dsort ${SUBJ}.phase.pb09_detrended_masked.nii.gz \
 	-polort -1 -mask ${SUBJ}_mask_base.nii.gz -prefix ${SUBJ}.Lsqrs.magnitude_phdenoised.nii.gz -overwrite 
 	
-	
-	
 	# Computing the Sums Of Squares of both denoised and preprocessed magnitude datasets
 	3dTstat -sos -prefix rm.${SUBJ}.Lsqrs.magnitude_phdenoised_sos.nii.gz ${SUBJ}.Lsqrs.magnitude_phdenoised.nii.gz -overwrite
-	
-	# Use only if mean is included in magnitude
-	#3dTproject -polort 0 \   # polort 0 if mean is included in magnitude!!! 
-	#-mask ${SUBJ}_mask_base.nii.gz -prefix rm.${SUBJ}.Lsqrs.magnitude_demean.nii.gz \
-	#-input ${SUBJ}.magnitude.pb04_volreg_detrended_mean_masked.nii.gz -overwrite
-	
-	
+		
 	3dTstat -sos -prefix rm.${SUBJ}.Lsqrs.magnitude.detrended_masked_sos.nii.gz \
 		${SUBJ}.magnitude.pb03_volreg_detrended_masked.nii.gz -overwrite
 	
@@ -140,7 +125,6 @@ Lsqrs() {
 
 	rm rm.${SUBJ}.Lsqrs.magnitude.detrended_masked_sos.nii.gz
 	rm rm.${SUBJ}.Lsqrs.magnitude_phdenoised_sos.nii.gz
-	#rm rm.${SUBJ}.Lsqrs.magnitude_demean.nii.gz
 
 	echo -e "\e[34m +++ ==================================================================================\e[39m"
 	echo -e "\e[34m +++ ---------------> END OF LEAST SQUARES REGRESSION WITH 3DTPROJECT <-----------------\e[39m"
@@ -148,6 +132,6 @@ Lsqrs() {
 
 }
 
-#Lsqrs
+OLS
 
 
